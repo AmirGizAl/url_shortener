7f3007b40066cf27ddbaa71b7661fb7a539a305a
agizatullin <agizatullin@axbit.ru>
2024-01-22 17:17:06 +0300

    Добавлены автотесты

--- test/controllers/urls_controller_test.rb
+++ test/controllers/urls_controller_test.rb
@@ -1,7 +1,56 @@
-require "test_helper"
+require 'test_helper'
 
 class UrlsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create url' do
+    post urls_url, params: { url: 'https://example.com' }
+    assert_response :created
+
+    response_body = JSON.parse(response.body)
+    assert_not_nil response_body['short_url']
+  end
+
+  test 'should handle create error' do
+    # Передайте неверный параметр, чтобы вызвать ошибку
+    post urls_url, params: { invalid_param: 'invalid_value' }
+    assert_response :unprocessable_entity
+
+    response_body = JSON.parse(response.body)
+    assert_not_nil response_body['error']
+  end
+
+  test 'should show url' do
+    Url.create(original_url: 'https://example.com', short_url: 'abc123')
+
+    get short_url_url(short_url: 'abc123')
+    assert_response :success
+
+    response_body = JSON.parse(response.body)
+    assert_equal 'https://example.com', response_body['original_url']
+  end
+
+  test 'should handle show error' do
+    get short_url_url(short_url: 'nonexistent')
+    assert_response :not_found
+
+    response_body = JSON.parse(response.body)
+    assert_not_nil response_body['error']
+  end
+
+  test 'should show url stats' do
+    Url.create(original_url: 'https://example.com', short_url: 'abc123', clicks: 10)
+
+    get url_stats_url(short_url: 'abc123')
+    assert_response :success
+
+    response_body = JSON.parse(response.body)
+    assert_equal 10, response_body['clicks']
+  end
+
+  test 'should handle stats error' do
+    get url_stats_url(short_url: 'nonexistent')
+    assert_response :not_found
+
+    response_body = JSON.parse(response.body)
+    assert_not_nil response_body['error']
+  end
 end
--- test/models/url_test.rb
+++ test/models/url_test.rb
@@ -1,7 +1,34 @@
-require "test_helper"
+require 'test_helper'
 
 class UrlTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid with valid attributes' do
+    url = Url.new(original_url: 'https://example.com')
+    assert url.valid?
+  end
+
+  test 'should be invalid with non-unique short_url' do
+    Url.create(original_url: 'https://example.com', short_url: 'abc123')
+    url = Url.new(original_url: 'https://example.org', short_url: 'abc123')
+    assert_not url.valid?
+    assert_equal ['has already been taken'], url.errors[:short_url]
+  end
+
+  test 'should generate short_url before validation' do
+    url = Url.new(original_url: 'https://example.com')
+    url.valid?
+    assert_not_nil url.short_url
+  end
+
+  test 'should raise error for invalid original_url' do
+    assert_raises URI::InvalidURIError do
+      Url.create(original_url: 'invalid_url')
+    end
+  end
+
+  test 'should raise error for non-HTTP original_url' do
+    assert_raises URI::InvalidURIError do
+      Url.create(original_url: 'ftp://example.com')
+    end
+  end
 end
+
